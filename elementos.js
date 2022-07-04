853bcc550499d0029fc978b4bccd28bd1a8ef42e
Cristian060999 <108699782+Cristian060999@users.noreply.github.com>
2022-07-04 17:19:39 -0500

    Create elementos.js

---
+++ elementos.js
@@ -0,0 +1,12 @@
+let registroNombre = document.getElementById("registroNombre");
+let registroContraseña = document.getElementById("registroContraseña");
+
+let quienSomos = document.getElementById("quienSomos");
+let registro = document.getElementById("registro");
+let productos = document.getElementById("productos");
+
+let categoriaLicores = document.getElementById("categoriaLicores");
+let licores = document.getElementById("licores");
+
+let venta = document.getElementById("venta");
+let btnComprar = document.getElementById("btnComprar");
